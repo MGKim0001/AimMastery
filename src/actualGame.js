25dd1f2a2227aba1c4c44cc0a9b564fc412b6c38
Bruce <wang2226@purdue.edu>
2017-11-20 19:54:16 -0600

    server working now, added crosshair

--- src/actualGame.js
+++ src/actualGame.js
@@ -1,21 +1,22 @@
+
 //global variable
 var canvas;
 var canvasContext;
-	
+
 var ballX = 50;
 var ballY = 50;
-    
+
 var ballSpeedX = 10;
 var ballSpeedY = 4;
-	
+
 var cursorX = 50;
 var cursorY = 50;
-	
+
 var BALL_SIZE = 20;
 var CURSOR_SIZE = 25;
-	
+
 var score = 0;
-	
+
 function calculateMousePos(evt){
     var rect = canvas.getBoundingClientRect();
     var root = document.documentElement;
@@ -26,19 +27,19 @@ function calculateMousePos(evt){
         y: mouseY
     };
 }
-	
+
 window.onload = function(){
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-        
+
     var fps = 30;
-		
-    setInterval(function(){    
+
+    setInterval(function(){
 		moveEverything();
 		canvas.addEventListener("click",collsionDetect);
         drawEverything();
     },1000/fps);
-		
+
 	canvas.addEventListener('mousemove',
 		function(evt){
 			var mousePos = calculateMousePos(evt);
@@ -46,34 +47,34 @@ window.onload = function(){
 			cursorY = mousePos.y - (CURSOR_SIZE/2);
 		});
 }
-	
+
 function reset(){
 	ballX = Math.floor(Math.random()*1280)+1;
     ballY = Math.floor(Math.random()*720)+1;
 }
-	
+
 function collsionDetect(){
 	var dx = ballX - cursorX;
 	var dy = ballY - cursorY;
 	var distance = Math.sqrt(dx*dx + dy*dy);
-		
+
 	if(distance < BALL_SIZE+CURSOR_SIZE){
 		score++;
 		reset();
 	}
 }
-	
+
 function moveEverything(){
 	//move target randomly
 	ballX = ballX + ballSpeedX;
 	ballY = ballY + ballSpeedY;
-		
+
     //bounce on the right wall
     if(ballX > canvas.width){
         ballSpeedX = -ballSpeedX;
     }
     //bounce on the left wall
-    if(ballX < 0){    
+    if(ballX < 0){
         ballSpeedX = -ballSpeedX;
     }
     //bounce on the top wall
@@ -84,34 +85,35 @@ function moveEverything(){
     if(ballY > canvas.height){
         ballSpeedY = -ballSpeedY;
     }
-		
+
 	console.log(ballX);
 	console.log(ballY);
 }
-	
+
 function drawEverything(){
-    //canvas
+  //canvas
 	var img = new Image(1280,720);
-	img.src = "../image/dust2.jpg";
+
+	img.src = "./image/dust2.jpg";
 	canvasContext.drawImage(img,0,0);
-		
+
 	//ball
-    colorCircle(ballX,ballY,BALL_SIZE,'red');
-		
+    colorCircle(ballX,ballY,BALL_SIZE,'blue');
+
 	//mouse cursor
-	colorCircle(cursorX,cursorY,CURSOR_SIZE,'blue');
-		
+	//colorCircle(cursorX,cursorY,CURSOR_SIZE,'blue');
+
 	//print out score
 	printScore();
 }
-	
+
 function colorCircle(centerX,centerY,radius,drawColor){
     canvasContext.fillStyle=drawColor;
     canvasContext.beginPath();
     canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
     canvasContext.fill();
 }
-    
+
 function printScore(){
 	canvasContext.font="30px fantasy";
 	const msg = "score: " + score;
